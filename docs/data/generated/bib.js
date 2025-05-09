300d132dc7fe1f538f52c312bc4cb87b2e5e5b51
Chenjiajun0225 <kennethhc02@163.com>
2025-05-09 17:48:30 +0100

    Deploy survis project to GitHub Pages

---
+++ docs/data/generated/bib.js
@@ -0,0 +1,17 @@
+﻿const generatedBibEntries = {
+    "Beck2016Visual": {
+        "abstract": "Social media and social networking web sites have continued to gain supremacy in determining the student’s success in education. It captures the attention of students and their tutors over the years. A social  media network is only an electronic links amongst its users which turned out to be a habit for students, youngsters.",
+        "author": "Oye, N. D. and 2Ibrahim, Inuwa",
+        "doi": "https://doi.org/10.5121/ijcsit.2016.8403",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "keywords": "type:article, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
+        "number": "01",
+        "publisher": "IEEE",
+        "series": "TVCG",
+        "title": "THE VICES OF SOCIAL MEDIA ON STUDENTS SUCCESS AT THE   ADAMAWA STATE POLYTECHNIC, YOLA- NIGERIA ",
+        "type": "article",
+        "url": "https://aircconline.com/ijcsit/V8N4/8416ijcsit03.pdf",
+        "volume": "22",
+        "year": "2016"
+    }
+};

0021ff3c7b2387b65d99c6e4dba776400ef7f014
ChenJiajun0225 <kennethhc02@163.com>
2025-05-09 18:13:24 +0100

    Update bib.js

--- docs/data/generated/bib.js
+++ docs/data/generated/bib.js
@@ -1,4 +1,4 @@
-﻿const generatedBibEntries = {
+const generatedBibEntries = {
     "Beck2016Visual": {
         "abstract": "Social media and social networking web sites have continued to gain supremacy in determining the student’s success in education. It captures the attention of students and their tutors over the years. A social  media network is only an electronic links amongst its users which turned out to be a habit for students, youngsters.",
         "author": "Oye, N. D. and 2Ibrahim, Inuwa",
@@ -13,10 +13,8 @@
         "url": "https://aircconline.com/ijcsit/V8N4/8416ijcsit03.pdf",
         "volume": "22",
         "year": "2016"
-    }
-};
-const generatedBibEntries6 = {
-    "Beck2016Visual": {
+    } ,
+    "Jensen2021Feedback": {
         "abstract": "Technologies associated with online learning have led to many new feedback practices and expanded the meaning of feedback beyond the traditional focus on instructor comments, but conceptual work on online feedback has not followed. This paper investigates how online learning researchers understand feedback’s role in teaching and learning, and discusses how these understandings influence what research questions are asked, and what online feedback practices are recommended. ",
         "author": "Lasse X Jensen, Margaret Bearman, David Boud",
         "doi": "https://doi.org/10.1016/j.compedu.2021.104271",
